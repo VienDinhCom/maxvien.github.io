85178840638b28013d3524e014620ab47206c86a
Vien Dinh <maxvien@gmail.com>
2019-02-20 17:01:30 +0700

    Updates

---
+++ precache-manifest.badae0a82c25c5989151377072ea6f8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "847f6c1e35041f0916f2",
+    "url": "/static/js/main.847f6c1e.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "847f6c1e35041f0916f2",
+    "url": "/static/css/main.96be04eb.chunk.css"
+  },
+  {
+    "revision": "c72fafe9b89a4908a262f9bd0fe8ed65",
+    "url": "/index.html"
+  }
+];
